63b6d9a45ca2d4cf18f88babd8d03da87d394dab
Francisco <fcarlosmonteiro@gmail.com>
2025-04-23 14:51:21 -0300

    fix models

--- app/__init__.py
+++ app/__init__.py
@@ -9,20 +9,16 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__, template_folder='views/templates')
     
-    # Configuração do banco de dados
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carros.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev')
     
-    # Inicialização das extensões
     db.init_app(app)
     migrate.init_app(app, db)
     
-    # Registro das rotas
     from app.routes import main_bp
     app.register_blueprint(main_bp)
     
-    # Criação das tabelas do banco de dados
     with app.app_context():
         db.create_all()
     
